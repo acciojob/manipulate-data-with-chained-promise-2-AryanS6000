cfe4a119fc3b7f18c3b5d9584d7f2b3a5016419e
AryanS6000 <163299006+AryanS6000@users.noreply.github.com>
2025-08-20 17:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+ function getarray(){
+	 return new Promise((resolve) => {
+		       setTimeOut(()=>{
+				     let arr = [1,2,3,4,5,6];
+				      resolve(arr);
+			   });
+	 } );
+ }
+ getarray().then((arr){
+	 return new Promise((resolve) =>{
+		 setTimeOut(()=>{
+			 const evens = arr.filter(num => num%2===0);
+			 document.getElementById("output").textContent = evens;
+			 resolve(evens);
+		 }, 1000);
+	 });
+ }).then((evens) =>{
+	   return new Promise((resolve) =>{
+		   setTimeOut(() =>{
+			   const doubled = evens.map(num => num*2);
+			   document.getElementById("output").textContent = doubled;
+		   },2000)
+	   });
+ });
